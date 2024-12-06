70c9f6c15ed9e4c505db7d293496c63180c9eca4
Shahid-khan015 <khan.shahidljpit2020@gmail.com>
2024-12-06 23:51:59 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response 
+from flask import Flask, Response , jsonify
 import os
 from flask_cors import CORS 
 
@@ -8,25 +8,29 @@ CORS(app)
 
 application = app
 
+out_dir = r'website/DataSet/Output/x'
+video_extensions = ('.mp4', '.avi', '.mov', '.mkv')
+video_links = {}
+links = [os.path.join(out_dir, f) for f in os.listdir(out_dir) if f.endswith(video_extensions)]
+for i in range(1 ,len(links)):
+    video_links[i] = links[i]
+
+
+@app.route('/')
+def home():
+    return jsonify(video_links)
+
+
+
 @app.route('/<int:number>')
 def render(number):
-
-    out_dir = r'website/DataSet/Output/x'
-    video_extensions = ('.mp4', '.avi', '.mov', '.mkv')
-    video_links = {}
-    links = [os.path.join(out_dir, f) for f in os.listdir(
-        out_dir) if f.endswith(video_extensions)]
-
-    for i in range(1 ,len(links)):
-        video_links[i] = links[i]
-
-        def generate(video_path):
-            with open(video_path, 'rb') as f:
-                while True:
-                    chunk = f.read(1024 * 1024)
-                    if not chunk:
-                        break
-                    yield chunk
+    def generate(video_path):
+        with open(video_path, 'rb') as f:
+            while True:
+                chunk = f.read(1024 * 1024)
+                if not chunk:
+                    break
+                yield chunk
 
     return Response(generate(video_links[number]), mimetype='video/mp4')
 
